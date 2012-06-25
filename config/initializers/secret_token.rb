1da7147bb4bdd18312ebf816d4a3c97b9136244f
Martin Kleppmann <martin@rapportive.com>
2012-06-24 20:01:39 -0700

    Autogenerated Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Octokey::Application.config.secret_token = '6e586d482ce431cc61ce381a3836ed3a7361902287b0155e1bbebe176df848f87340677f7455fe7d85cdaa384fe6297b8e304fafd05ec1f8fb9fc3dea837e4d6'
