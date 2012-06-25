8ea3246dcebbbb64debfb3b47f3218d1d6765712
Martin Kleppmann <martin@rapportive.com>
2012-06-24 22:10:35 -0700

    Hello world with Twitter bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Octokey::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'website#index'
 
   # See how all your routes lay out with "rake routes"
 
